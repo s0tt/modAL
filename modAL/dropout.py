441dbe9c4884233423c18079a0422f61b41824d8
maxkeller321 <max-knobloch2312@outlook.de>
2021-04-23 10:23:54 +0200

    cpu_conversion_change

--- modAL/dropout.py
+++ modAL/dropout.py
@@ -329,9 +329,9 @@ def get_predictions(classifier: BaseEstimator, X: modALinput, dropout_layer_inde
             logger.info("Time for logits_adaptor, with {} samples: {}".format(sample_per_forward_pass, time.time()- time_logits_adaptor_before))
 
             mask = ~prediction.isnan()
-            prediction[mask] = prediction[mask].unsqueeze(0).softmax(1)
+            prediction[mask] = prediction[mask].unsqueeze(0).softmax(1).cpu()
             if probas is None: probas = torch.empty((number_of_samples, prediction.shape[-1]), device='cpu')
-            probas[range(sample_per_forward_pass*index, sample_per_forward_pass*(index+1)), :] = prediction.cpu()
+            probas[range(sample_per_forward_pass*index, sample_per_forward_pass*(index+1)), :] = prediction
             logger.info("Time for full_prediction_cycle, with {} samples: {}".format(sample_per_forward_pass, time.time()- time_before_infer))
 
 
